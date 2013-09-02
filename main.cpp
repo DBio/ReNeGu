d02087731ac5c22a7752e2359e15cce685c46489
Adam Streck <adam.streck@gmail.com>
2013-09-02 18:06:08 +0200

    Works now, would use some refactoring.

--- main.cpp
+++ main.cpp
@@ -27,12 +27,12 @@ size_t findDAColumn(const vector<string> & column_names) {
 string obtainName(const string & column_name) {
     if (regex_match(column_name, regex("DV:.*")))
         return column_name.substr(3, column_name.size() - 3);
-    if (regex_match(column_name, regex("TR:.*(i|a)")))
-        return column_name.substr(3, column_name.size() - 4);
     if (regex_match(column_name, regex("TR:.*:Inhibitors")))
         return column_name.substr(3, column_name.size() - 3 - strlen("Inhibitors") - 1);
-    if (regex_match(column_name, regex("TR:.*:Activators")))
+    if (regex_match(column_name, regex("TR:.*:Stimuli")))
         return column_name.substr(3, column_name.size() - 3 - strlen("Activators") - 1);
+    if (regex_match(column_name, regex("TR:.*(i|a)")))
+        return column_name.substr(3, column_name.size() - 4);
     return "";
 }
 
@@ -88,16 +88,20 @@ vector<ComponentData> getComponenets(const vector<string> & column_names) {
 vector<vector<vector<double> > > getTimepoints(const vector<ComponentData> & components, const size_t DA_colum, fstream & input_file) {
     vector<vector<vector<double> > > timepoints; // Measurements are grouped by a timepoint, not by the experiment!
     timepoints.push_back(vector<vector<double> >());
-    string measurement_time = "0";
+    string measurement_time = "";
     size_t time_point = 0;
     string data_line;
     while(getline(input_file, data_line)) {
         vector<string> data;
         boost::split(data, data_line, boost::is_any_of(","));
         if (measurement_time.compare(data[DA_colum]) != 0) {
-            measurement_time = data[DA_colum];
-            time_point++;
-            timepoints.push_back(vector<vector<double> >());
+            if (measurement_time.empty())
+                measurement_time = data[DA_colum];
+            else {
+                measurement_time = data[DA_colum];
+                time_point++;
+                timepoints.push_back(vector<vector<double> >());
+            }
         }
         vector<double> values;
         for (const ComponentData & component : components) {
@@ -177,12 +181,12 @@ struct Interval {
         for (const size_t val_no : scope(first)) {
             const double change = second[val_no] - first[val_no];
             changes.push_back(change);
-            if (step > 0.) {
+            if ((change) > 0.) {
                 avg_production += change;
                 production_cases++;
             }
         }
-        avg_production /= avg_production;
+        avg_production /= production_cases;
 
         // Compute production factor - positive if bigger than average, negative otherwise
         for (const double change : changes) {
@@ -208,85 +212,114 @@ vector<Interval> makeIntervals(const vector<vector<vector<double> > > & values) 
 
 TEST_F(DataTest, MakeIntervals) {
     vector<Interval> intervals = makeIntervals(normalize(data1));
-    EXPECT_DOUBLE_EQ(0.5, intervals[0].change[0]);
-    EXPECT_DOUBLE_EQ(1., intervals[0].change[1]);
-    EXPECT_DOUBLE_EQ(-0.75, intervals[1].change[0]);
-    EXPECT_DOUBLE_EQ(-0.25, intervals[1].change[1]);
+    EXPECT_DOUBLE_EQ(0.5, intervals[0].changes[0]);
+    EXPECT_DOUBLE_EQ(1., intervals[0].changes[1]);
+    EXPECT_DOUBLE_EQ(-0.75, intervals[1].changes[0]);
+    EXPECT_DOUBLE_EQ(-0.25, intervals[1].changes[1]);
 }
 
 double inline logistic(const double x) {
-    return 1 / (1 + exp(-x));
+    return 1 / (1 + exp(-(x*12-6)));
 }
 
 double inline logistic(const double x, const double y) {
-    return logistic(x) + logistic(y) - logistic(x)*logistic(y);
+    return logistic((x+y)/2);
 }
 
 double inline logistic(const double x, const double y, const double z) {
-    return logistic(x) + logistic(y) + logistic(z)
-           - logistic(x)*logistic(y) - logistic(x)*logistic(z) - logistic(y)*logistic(z)
-           + logistic(x)*logistic(y)*logistic(z);
+    return logistic((x+y+z)/3);
 }
 
+TEST(FunctionTest, logistic) {
+    cout << logistic(0.5) << " " << logistic(0.5, 0.) << " " << logistic(0.5, 0.,0.) << "\n";
+    cout << logistic(0.5) << " " << logistic(0.5, 0.5) << " " << logistic(0.5, 0.5, 0.5) << "\n";
+    cout << logistic(0.5) << " " << logistic(0.5, 1.) << " " << logistic(0.5, 1.,1.) << "\n";
+    cout << logistic(1.) << " " << logistic(1., 1.) << " " << logistic(1., 1.,1.) << "\n";
+    cout << logistic(0.1,0.9) << " " << logistic(0.5,0.5) << "\n";
+}
 
-vector<ComponentData> computeRegulators(const vector<ComponentData> & components, vector<Interval> & intervals) {
+vector<ComponentData> computeInhibitors(const vector<ComponentData> & components, vector<Interval> & intervals) {
     vector<ComponentData> regulated = components;
+    const size_t total = pow(components.size(), 3) * count_if(components.begin(), components.end(), [](const ComponentData & comp){return comp.measured;});
+    size_t current = 0;
 
     for (const size_t target_no : scope(components)) {
         map<double, set<string> > mutual_inf;
         if (!components[target_no].measured)
             continue;
 
-        // Test single inhibitors.
-        for (const size_t source_no : scope(components)) {
-            double effect = 0.;
-            for (const Interval & interval : intervals) {
-                effect += -1 * interval.changes[target_no] * logistic(interval.first_[source_no]);
-            }
-            effect /= intervals.size();
-            mutual_inf.insert(make_pair(effect, set<string>({components[source_no].name})));
-        }
-
-        // Test two inhibitors.
+        // Test all combinations up to three inhibitors (combination of multiple selfs == just single one).
         for (const size_t source_1_no : scope(components)) {
-            for (const size_t source_2_no : range(source_1_no + 1, components.size())) {
-                double effect = 0.;
-                for (const Interval & interval : intervals) {
-                    effect += -1 * interval.changes[target_no] * logistic(interval.first_[source_1_no],interval.first_[source_2_no]);
-                }
-                effect /= intervals.size();
-                mutual_inf.insert(make_pair(effect, set<string>({components[source_1_no].name, components[source_2_no].name})));
-            }
-        }
-
-        // Test three inhibitors.
-        for (const size_t source_1_no : scope(components)) {
-            for (const size_t source_2_no : range(source_1_no + 1, components.size())) {
-                for (const size_t source_3_no : range(source_1_no + 1, components.size())) {
+            for (const size_t source_2_no : scope(components)) {
+                for (const size_t source_3_no : scope(components)) {
+                    cout << "Testing inhibitions: " << current++ << "/" << total << "\r";
                     double effect = 0.;
                     for (const Interval & interval : intervals) {
                         effect += -1 * interval.changes[target_no] * logistic(interval.first_[source_1_no],interval.first_[source_2_no],interval.first_[source_3_no]);
                     }
-                    effect /= intervals.size();
                     mutual_inf.insert(make_pair(effect, set<string>({components[source_1_no].name, components[source_2_no].name, components[source_3_no].name})));
                 }
             }
         }
 
-
         auto max_it = max_element(mutual_inf.begin(), mutual_inf.end());
         // Pick the one with the biggest positive influence - if there is none, just take empty.
-        regulated[target_no].inhibitors = max_it->first > 0 ? max_it->second : set<string>();
+        regulated[target_no].inhibitors = (max_it->first - 0.05* max_it->second.size() ) > 0 ? max_it->second : set<string>();
     }
 
     return regulated;
 }
 
+vector<ComponentData> computeActivators(const vector<ComponentData> & components, vector<Interval> & intervals) {
+    vector<ComponentData> regulated = components;
+    const size_t total = pow(components.size(), 3) * count_if(components.begin(), components.end(), [](const ComponentData & comp){return comp.measured;});
+    size_t current = 0;
+
+    for (const size_t target_no : scope(components)) {
+        map<double, set<string> > mutual_inf;
+        if (!components[target_no].measured)
+            continue;
+
+        // Test all combinations up to three inhibitors (combination of multiple selfs == just single one).
+        for (const size_t source_1_no : scope(components)) {
+            for (const size_t source_2_no : scope(components)) {
+                for (const size_t source_3_no : scope(components)) {
+                     cout << "Testing activators: " << current++ << "/" << total << "\r";
+                    double effect = 0.;
+                    for (const Interval & interval : intervals) {
+                        effect += interval.productions[target_no] * (interval.first_[source_1_no],interval.first_[source_2_no],interval.first_[source_3_no]);
+                    }
+                    mutual_inf.insert(make_pair(effect, set<string>({components[source_1_no].name, components[source_2_no].name, components[source_3_no].name})));
+                }
+            }
+        }
+
+        auto max_it = max_element(mutual_inf.begin(), mutual_inf.end());
+        // Pick the one with the biggest positive influence - if there is none, just take empty.
+        regulated[target_no].activators = (max_it->first - 0.05* max_it->second.size() ) > 0 ? max_it->second : set<string>();
+    }
+
+    return regulated;
+}
+
+void output(const vector<ComponentData> & components, const string & file_name) {
+    ofstream fout(file_name + ".sif", ios::out);
+
+    for (const ComponentData & component: components) {
+        for (const string & source : component.activators) {
+            fout << source << " 1 " << component.name << endl;
+        }
+        for (const string & source : component.inhibitors) {
+            fout << source << " -1 " << component.name << endl;
+        }
+    }
+}
+
 int main(int argc, char* argv[]) {
-    // if (argc < 2) {
+    if (argc < 2) {
         ::testing::InitGoogleTest( &argc, argv );
-        RUN_ALL_TESTS();
-    // }
+        return RUN_ALL_TESTS();
+    }
 
     fstream input_file(argv[1], ios::in);
     if (!input_file)
@@ -303,7 +336,19 @@ int main(int argc, char* argv[]) {
     vector<vector<vector<double> > > timepoints = getTimepoints(components, DA_colum, input_file);
     timepoints = normalize(timepoints);
     vector<Interval> intervals = makeIntervals(timepoints);
-    components = computeRegulators(components, intervals);
+    components = computeInhibitors(components, intervals);
+    components = computeActivators(components, intervals);
+
+    string name;
+    for (const char ch : string(argv[1])) {
+        if (ch == '.')
+            break;
+        if (ch == '/' || ch == '\\')
+            name.clear();
+        name.push_back(ch);
+    }
+
+    output(components, name);
 
     return 0;
 }
